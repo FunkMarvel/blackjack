43806e7c77003ece542997d1e28a5d3d99691532
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2021-10-12 19:55:55 +0200

    Update casino.h
    added forward declarations of userDraws and houseDraws functions.

--- blackjack/casino.h
+++ blackjack/casino.h
@@ -5,3 +5,5 @@
 void clearCin();
 void mainMenu(User&);
 void blackjack(User&);
+bool userDraws(User& current_user, vector<Card>& deck, int& num_cards_pulled);
+void houseDraws(User& current_user, vector<Card>& deck, int& num_cards_pulled);
