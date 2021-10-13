78a60e805157e68a08ad1b31f9a739a41a4f7c0e
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2021-10-13 23:45:05 +0200

    Added comments and doc-strings to card.cpp and card.h

--- blackjack/card.cpp
+++ blackjack/card.cpp
@@ -1,48 +1,67 @@
-#include "card.h"
+#include "casino.h"
 #include <random>
 #include <algorithm>
 
 Card::Card() {
-	colour = 's'; value = 1;
+	// Default constructor for Card-struct. Default card is ace of spades.
+	colour = 3; rank = 1;
 }
 
-Card::Card(int in_colour, int in_value) {
+Card::Card(int in_colour, int in_rank) {
+	// constructor that gives card user-provided colour and rank.
 	colour = in_colour;
-	value = in_value;
+	rank = in_rank;
 }
 
 std::ostream& operator <<(std::ostream& os, Card some_card) {
-	vector<string> colours{ "hearts", "diamonds", "clubs", "spades" };
-	vector<string> value{ "ace", "knight", "queen", "king" };
+	// overloading insertion operator for Card struct. Inserts string with Card-object's
+	// colour and rank into ostream with format "[rank] of [colour]".
+	// 
+	// Args:
+	//	os -- refrence to ostream.
+	//	some_card -- object of struct Card.
 
-	if (some_card.value == 1) {
-		os << value[some_card.value - 1] << " of " << colours[some_card.colour];
+	// vectors with colour-names and special ranks:
+	vector<string> colours{ "hearts", "diamonds", "clubs", "spades" };
+	vector<string> ranks{ "ace", "knight", "queen", "king" };
+
+	if (some_card.rank == 1) {  // formats for aces.
+		os << ranks[some_card.rank - 1] << " of " << colours[some_card.colour];
 		return os;
 	}
-	else if (some_card.value > 10) {
-		os << value[some_card.value - 10] << " of " << colours[some_card.colour];
+	else if (some_card.rank > 10) {  // formats for knights, queens and kings:
+		os << ranks[some_card.rank - 10] << " of " << colours[some_card.colour];
 		return os;
 	}
-	else {
-		os << some_card.value << " of " << colours[some_card.colour];
+	else {  // formats for all other cards:
+		os << some_card.rank << " of " << colours[some_card.colour];
 		return os;
 	}
 }
 
 vector<Card> createDeck() {
+	// Function creating and returning an ordered vector of Card-objects.
+	// The vector corresponds to a regular 52 card playing deck.
+
 	vector<Card> deck{};
 
 	for (int i = 0; i < 4; i++) {
 		for (int j = 1; j <= 13; j++) {
-			deck.push_back(Card(i, j));
+			deck.push_back(Card(i, j));  // i loops through colours and j loops through ranks.
 		}
 	}
 	return deck;
 }
 
 void shuffleDeck(vector<Card> &deck) {
+	// Function shuffling the provided vector of cards, using <algorithm>'s shuffle function.
+	//
+	// Args:
+	//	deck -- refrence to vector of Card-objects.
+
+	// using 64-bit Mersenne-Twister as RNG for shuffle-function:
 	std::random_device rd{};
 	std::mt19937_64 gen(rd());
 
-	std::shuffle(deck.begin(), deck.end(), gen);
+	std::shuffle(deck.begin(), deck.end(), gen);  // shuffling deck.
 }
--- blackjack/card.h
+++ blackjack/card.h
@@ -1,20 +1,33 @@
 #pragma once
+
+// This is the top-level header, so here are the top-level includes:
 #include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
 
+// namespacing commonly used names:
 using std::string; using std::cout; using std::cin;
 using std::vector; using std::endl;
 
 struct Card {
-	int colour{};
-	int value{};
+	// Struct corresponding to a single playing card.
+	// Each Card object has a colour (0 == hearts, 1 == diamonds, 2 == clubs, 3 == spades),
+	// and a rank (number from and including 1 to and including 13).
+	// Rank 1 corresponds to ace, and rank 11, 12 and 13 corresponds to
+	// knight, queen and king respectivly.
 
+	int colour{};
+	int rank{};
+
+	// constructors:
 	Card();
 	Card(int, int);
+
+	// operator overloads:
 	friend std::ostream& operator <<(std::ostream&, Card);
 };
 
+// stand-alone functions for creating and shuffeling deck of cards:
 vector<Card> createDeck();
 void shuffleDeck(vector<Card>&);
